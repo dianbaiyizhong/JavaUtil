121a373da9f02a2cdedf16826fbbe510affa4479
huanghaoming <1585685035@qq.com>
2021-04-14 21:49:57 +0800

    提交

--- src/test/java/TestRxJava.java
+++ src/test/java/TestRxJava.java
@@ -1,82 +1,99 @@
-import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.core.ObservableEmitter;
 import io.reactivex.rxjava3.core.ObservableOnSubscribe;
+import io.reactivex.rxjava3.functions.BiFunction;
+import io.reactivex.rxjava3.functions.Consumer;
 import io.reactivex.rxjava3.schedulers.Schedulers;
+import org.junit.After;
 import org.junit.Test;
-
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 
 public class TestRxJava {
 
-    public static void main(String[] args) {
-        final Observable<String> memory = Observable.create(new ObservableOnSubscribe<String>() {
 
-            @Override
-            public void subscribe(@NonNull ObservableEmitter<String> observableEmitter) throws Throwable {
+    /**
+     * 前面任何一个满足条件，后面都不会执行
+     */
+    @Test
+    public void testConcat() {
 
-                observableEmitter.onComplete();
-            }
+        Observable<String> memory = Observable.create((ObservableOnSubscribe<String>) emitter -> {
+            // emitter.onComplete();
+            emitter.onNext("memory");
         }).subscribeOn(Schedulers.io());
-        final Observable<String> disk = Observable.create(new ObservableOnSubscribe<String>() {
+        Observable<String> disk = Observable.create((ObservableOnSubscribe<String>) emitter -> {
+            // emitter.onComplete();
+            emitter.onNext("disk");
 
-            @Override
-            public void subscribe(@NonNull ObservableEmitter<String> observableEmitter) throws Throwable {
-
-//                observableEmitter.onNext("disk");
-                observableEmitter.onComplete();
-
-            }
         }).subscribeOn(Schedulers.io());
 
-        Observable<String> network = Observable.just("network");
 
-
-        //主要就是靠concat operator来实现
-        Observable.concat(memory, disk, network)
+        Observable.concat(memory, disk)
                 .subscribeOn(Schedulers.newThread())
                 .subscribe(s -> {
-                    System.out.println("--------------subscribe: " + s);
+                    System.out.println(s);
                 });
 
 
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 
+
+    /**
+     * 将多个事件合并成一个
+     */
     @Test
-    public void test1() {
-        //https://blog.csdn.net/finalheart/article/details/87615546
-        CompletableFuture completableFuture = new CompletableFuture<>();
+    public void testMerge() {
+        Observable<String> memory = Observable.create((ObservableOnSubscribe<String>) emitter -> {
+            // emitter.onComplete();
+            emitter.onNext("memory");
+        }).subscribeOn(Schedulers.newThread());
+        Observable<String> disk = Observable.create((ObservableOnSubscribe<String>) emitter -> {
+            // emitter.onComplete();
+            Thread.sleep(5000);
+            emitter.onNext("disk");
+        }).subscribeOn(Schedulers.newThread());
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
+        Observable.merge(memory, disk)
+                .subscribeOn(Schedulers.newThread())
+                .subscribe(s -> {
+                    System.out.println(s);
+                });
+    }
 
-                try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                completableFuture.complete("s");
 
-            }
+    /**
+     * 等待多个任务执行结束
+     */
+    @Test
+    public void testZip() {
+        Observable<String> memory = Observable.create((ObservableOnSubscribe<String>) emitter -> {
+            // emitter.onComplete();
+            emitter.onNext("memory");
+        }).subscribeOn(Schedulers.newThread());
+        Observable<String> disk = Observable.create((ObservableOnSubscribe<String>) emitter -> {
+            // emitter.onComplete();
+            Thread.sleep(5000);
+            emitter.onNext("disk");
+        }).subscribeOn(Schedulers.newThread());
 
-        }).start();
+        Observable.zip(memory, disk, (a, b) -> a + b).subscribe(o ->
+                System.out.println(o)
+        );
+    }
 
-        try {
-            System.out.println(completableFuture.get());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
 
+    @Test
+    public void testOther() {
+        Observable.just("1", "2", "2", "3", "4", "5")
+                .map(Integer::parseInt)
+                .filter(s -> s > 1)
+                .distinct()
+                .take(3)  // 只要前3项
+                .reduce((integer, integer2) -> integer.intValue() + integer2.intValue())
+                .subscribe(System.out::println);
+    }
+
+    @After
+    public void sleep() {
         try {
             Thread.sleep(10000);
         } catch (InterruptedException e) {
