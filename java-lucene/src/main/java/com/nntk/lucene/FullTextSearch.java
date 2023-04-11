d1bed039a8dfee1516d6508f91114b08b3457234
huanghaoming <1585685035@qq.com>
2023-04-11 21:53:05 +0800

    提交

--- java-lucene/src/main/java/com/nntk/lucene/FullTextSearch.java
+++ java-lucene/src/main/java/com/nntk/lucene/FullTextSearch.java
@@ -3,6 +3,7 @@ package com.nntk.lucene;
 import cn.hutool.db.Db;
 import cn.hutool.db.Entity;
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.TextField;
@@ -15,6 +16,7 @@ import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.ByteBuffersDirectory;
 import org.apache.lucene.store.Directory;
 import org.wltea.analyzer.lucene.IKAnalyzer;
@@ -29,9 +31,9 @@ public class FullTextSearch {
 //        Analyzer analyzer = new StandardAnalyzer();
         Analyzer analyzer = new IKAnalyzer(true);
 
-        Directory index = new ByteBuffersDirectory();
+        Directory directory = new ByteBuffersDirectory();
         IndexWriterConfig config = new IndexWriterConfig(analyzer);
-        IndexWriter writer = new IndexWriter(index, config);
+        IndexWriter writer = new IndexWriter(directory, config);
         List<Entity> allBank = Db.use().query("select * from t_question_bank");
 
         for (int i = 0; i < allBank.size(); i++) {
@@ -44,20 +46,23 @@ public class FullTextSearch {
         }
         writer.close();
 
-        String keyword = "御前科举大赛第3关：这一关考的是古代工艺。题目：君山银针属于";
-        Query query = new QueryParser("question", analyzer).parse(keyword);
-
-
-        IndexReader reader = DirectoryReader.open(index);
+        IndexReader reader = DirectoryReader.open(directory);
         IndexSearcher searcher = new IndexSearcher(reader);
-        int numberPerPage = 1000;
+
+
+        String keyword = "礼部考题，已答1题，答对1题。长安城中有一处地方可以为人疗伤，这个地方是";
+        Query query = new QueryParser("question", analyzer).parse(QueryParser.escape(keyword));
+
+
+        int numberPerPage = 10;
         System.out.printf("查询关键字是：\"%s\"%n", keyword);
         ScoreDoc[] hits = searcher.search(query, numberPerPage).scoreDocs;
+
 
         for (int i = 0; i < hits.length; i++) {
             ScoreDoc hit = hits[i];
             Document doc = searcher.doc(hit.doc);
-            System.out.println(hit.score + "====" + doc.getField("question"));
+            System.out.println(hit.score + "====" + doc.get("question") + "=====" + doc.get("answer"));
         }
         System.out.println(hits.length);
     }
